f3e86e8d1a844b1a0570b83ee488a0552fe2b255
Dishaan <ds@rapid.dk>
2025-03-09 19:08:57 +0100

    page setup

---
+++ script.js
@@ -0,0 +1,39 @@
+const correctText = "jessie er helten";
+const successGifUrl = "tillykke.png";
+const failureGifUrl = "wopwop.png";
+
+function checkText() {
+    const input = document.getElementById('secretInput').value.toLowerCase();
+    const gifContainer = document.getElementById('gif-container');
+    const resultGif = document.getElementById('resultGif');
+    const inputSection = document.getElementById('input-section');
+    const successSection = document.getElementById('success-section');
+
+    // Remove any existing animation classes
+    gifContainer.classList.remove('slide-in');
+    gifContainer.classList.remove('hidden');
+
+    // Force reflow
+    void gifContainer.offsetWidth;
+
+    if (input === correctText) {
+        resultGif.src = successGifUrl;
+        gifContainer.classList.add('slide-in');
+        
+        // Show success section after animation
+        setTimeout(() => {
+            inputSection.classList.add('hidden');
+            successSection.classList.remove('hidden');
+            successSection.classList.add('slide-in');
+        }, 2000);
+    } else {
+        resultGif.src = failureGifUrl;
+        gifContainer.classList.add('slide-in');
+        
+        // Reset after animation
+        setTimeout(() => {
+            gifContainer.classList.add('hidden');
+            document.getElementById('secretInput').value = '';
+        }, 2000);
+    }
+} 
